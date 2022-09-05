c2b409f891bfab50994d5296db90a99f502481d9
Bethel Arthur <kbarthur14@gmail.com>
2022-09-05 16:49:37 -0500

    Add Search Component

--- src/App.js
+++ src/App.js
@@ -3,35 +3,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
-
+import SearchBox from './components/SearchBox';
 
 function App() {
-
 	const [movies, setMovies] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async () => {
-		const url = "http://www.omdbapi.com/?s=batman&apikey=c5d1db1f"
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=c5d1db1f`
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
 
-		setMovies(responseJson.Search);
+		if (responseJson.Search) {
+			setMovies(responseJson.Search)
+		}
 	};
 
 	useEffect(() => {
-		getMovieRequest();
-	}, []);
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
 
 	return (
 		<div className='container-fluid movie-app'>
-			<div className='row'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
 				<MovieListHeading heading="Movies" />
+				<SearchBox searhValue={searchValue} setSearchValue={setSearchValue} />
 			</div>
 			<div className='row'>
 				<MovieList movies={movies} />
 			</div>
 		</div>
 	);
-}
+};
 
 export default App;
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,10 +4,10 @@ const MovieList = (props) => {
     return (
         <>
             {props.movies.map((movie, index) => (
-                <div>
+                <div className="d-flex justify-content-start m-3">
                     <img src={movie.Poster} alt='movie'></img>
                 </div>
-            ))};
+            ))}
         </>
     );
 };
